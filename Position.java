6d94d232d0980d390efdb6e31041084f8205644d
Oliver Zhu <30608978+OliverZhu7@users.noreply.github.com>
2017-08-07 21:21:27 -0400

    Add files via upload

---
+++ Position.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package musicsurvey;
+
+/**
+ * @author zichen
+ * @version 2017.08.07
+ * there are always three position on the window, 
+ * so each section is associated with one of three position: TOP,
+ * MIDDLE, or BOTTOM
+ */
+public enum Position {
+    TOP, MIDDLE, BOTTOM, OTHER;
+}
+
