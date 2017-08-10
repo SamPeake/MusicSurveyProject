a7ab8cca6280f603eba199e677ae7667f1fa696a
SamPeake <samp97@vt.edu>
2017-08-10 19:43:26 -0400

    Add files via upload

--- PlayList.java
+++ PlayList.java
@@ -3,8 +3,10 @@
  */
 package musicsurvey;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.stream.Collector;
 
 /**
  * @author sampe
@@ -12,109 +14,29 @@ import java.util.LinkedList;
  */
 public class PlayList extends LinkedList<Song> {
 
-    
-    
     public PlayList() {
         super();
     }
     public void sortBySong() {
-        
-        
-        for (int i = 0; i < super.size(); i++) {
-            Song song1 = this.get(i);
-            Song song2 = song1;
-            int index = i;
-            for (int j = i + 1; j < super.size(); j++) {
-                song2 = super.get(j);
-                
-                if (song1.getSongName().compareTo(song2.getSongName()) > 0) {
-                    index = j;
-                }
 
-            }
-            if(index == i) {
-                song2 = song1;
-            }
-            super.remove(index);
-            super.add(index, song1);
-            super.remove(i);
-            super.add(i, song2);
-        }
-
+     
+        Collections.sort(this, new SongComparator());
 
     }
-    
 
     public void sortByArtist() {
 
-        for (int i = 0; i < super.size(); i++) {
-            Song song1 = super.get(i);
-            Song song2 = song1;
-            int index = i;
-            for (int j = i; j < super.size(); j++) {
-                song2 = super.get(j);
-                
-                if (song1.getArtist().compareTo(song2.getArtist()) < 0) {
-                    index = j;
-                }
-
-            }
-            if(index != i) {
-                super.remove(index);
-                super.add(index, song1);
-                super.remove(i);
-                super.add(i, song2);
-            }
-            
-        }
+        Collections.sort(this, new ArtistComparator());
     }
 
     public void sortByGenre() {
 
-        for (int i = 0; i < super.size(); i++) {
-            Song song1 = this.get(i);
-            Song song2 = song1;
-            int index = i;
-            for (int j = i; j < super.size(); j++) {
-                song2 = super.get(j);
-                
-                if (song1.getGenre().compareTo(song2.getGenre()) > 0) {
-                    index = j;
-                }
-
-            }
-            if(index == i) {
-                song2 = song1;
-            }
-            super.remove(index);
-            super.add(index, song1);
-            super.remove(i);
-            super.add(i, song2);
-        }
+        Collections.sort(this, new GenreComparator());
     }
 
     public void sortByYear() {
 
-        for (int i = 0; i < super.size(); i++) {
-            Song song1 = this.get(i);
-            Song song2 = song1;
-            int index = i;
-            for (int j = i; j < super.size(); j++) {
-                song2 = super.get(j);
-                
-                if ((song1.getYear() - song2.getYear()) > 0) {
-                    index = j;
-                }
-
-            }
-            if(index == i) {
-                song2 = song1;
-            }
-            super.remove(index);
-            super.add(index, song1);
-            super.remove(i);
-            super.add(i, song2);
-        }
+        Collections.sort(this, new YearComparator());
     }
 
     public String toSongString() {
