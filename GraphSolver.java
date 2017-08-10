642c70167124252be7e598611f3b8a105dd8d827
SamPeake <samp97@vt.edu>
2017-08-09 21:33:30 -0400

    Add files via upload

--- GraphSolver.java
+++ GraphSolver.java
@@ -1,135 +1,126 @@
+/**
+ * 
+ */
+package musicsurvey;
+
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
-
-import methods.Song;
+import java.util.Observable;
 
 /**
- * 
+ * @author sampe
+ *
  */
+public class GraphSolver extends Observable {
 
-/**
- * This creates the graphSolver class
- * 
- * @author Hytham Soueid (Hythams8)
- * @version 2017.08.07
- */
-public class GraphSolver {
-    // Fields
-    /**
-     * ReadFile field
-     */
-    private ReadFile reader;
-    /**
-     * Playlist field
-     */
     private LinkedList<Song> playList;
-    /**
-     * input field
-     */
-    private String input;
-    /**
-     * int field
-     */
     private int pageCounter;
-    /**
-     * student info
-     */
-    private String studentInfo;
-    /**
-     * song info
-     */
-    private String songInfo;
+    private BufferedReader songFile;
+    private BufferedReader surveyFile;
 
-    // Constructor
-    /**
-     * Constructor for the class
-     */
-    public GraphSolver(String studentInfo, String songInfo) {
+    public GraphSolver(String songFile, String surveyFile) {
 
-    }
-
-    // Methods
-    /**
-     * Creates the PlayList
-     */
-    public void makePlaylist() {
-
-    }
-
-    /**
-     * Makes the student
-     */
-    private Student makeStudent() {
-
-    }
-
-    /**
-     * Updates the song
-     */
-    private Song upDateSong(Student) {
-        
-    }
-
-    /**
-     * Updates the playlist
-     */
-    public void upDatePlaylist() {
-
-    }
-
-    /**
-     * Gets the next nine songs
-     */
-    public Song[] getNextNineSongs() {
-
-    }
-
-    /**
-     * Gets the previous nine songs
-     */
-    public Song[] getPrevNineSongs() {
-
-    }
-
-    /**
-     * Reads the file within the class
-     * 
-     * @param args
-     */
-    public String readFile() {
-        String csvFile = "MusicSurveyData2016S.csv";
-        String line = "";
-        String csvSplitBy = ",";
-        try (BufferedReader reader =
-                new BufferedReader(new FileReader(csvFile))) {
-            int index = 0;
-            while ((line = reader.readLine()) != null) {
-
-                // just reading the first row with the column names
-                if (index == 0) {
-                    System.out.println("first line:" + line);
-
-                    String[] tokens =
-                            line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-                    for (String t : tokens) {
-                        System.out.println("> " + t);
-                    }
-                }
-                else if (index > 0 & index < 15) {
-                    String[] responses = line.split(csvSplitBy);
-                    for (String t : responses) {
-                        System.out.println("response # " + index + ":" + t);
-                    }
-                }
-                index++;
-            }
-
+        try {
+            this.songFile = new BufferedReader(new FileReader(songFile));
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+        try {
+            this.surveyFile = new BufferedReader(new FileReader(surveyFile));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        playList = new LinkedList<Song>();
+        pageCounter = 1;
+    }
+
+    @SuppressWarnings("unchecked")
+    public String[] readSurveyFile(int index) {
+        String line = "";
+        String[] main;
+        try {
+            line = surveyFile.readLine();
+            if (line != null) {
+
+                String[] responses;
+                // just reading the first row with the column names
+                if (index == 0) {
+
+                    responses = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)",
+                            -1);
+
+                }
+                // survey responses ,
+                // limiting the index range so that system.out can show
+                // everything
+
+                else {
+                    responses = line.split(",");
+
+                }
+
+                return responses;
+            }
+            return null;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private Student makeStudent() {
+        int index = 1;
+        String[] row = readSurveyFile(index);
+        int rowNumber = Integer.parseInt(row[0]);
+        Student newStudent = new Student(row[2], row[3], row[4], rowNumber,
+                row[1]);
+        for (int i = 5; i < row.length; i++) {
+            newStudent.add(row[i]);
+        }
+        return newStudent;
 
     }
+    private void upDateSong(Student student) {
+        for(int i = 0; i < playList.getLength(); i++) {
+            int dim = 1;
+        //Hobby
+            dim = 2;
+        if (student.getHobby() == "reading") {
+            String answer = student.get((i * 2) + 5);
+            String answer2 = student.get((i * 2) + 6);
+            if ( answer == "Yes") {
+            playList.getEntry(i).getDimension("hobby").getCategory(1).upDateHeard();
+            }
+            else {
+                playList.getEntry(i).getDimension("hobby").getCategory(1).upDateTotal(); 
+            }
+            
+            if (answer == "Yes") {
+                playList.getEntry(i).getDimension("hobby").getCategory(1).upDateHeard();
+            }
+            
+            
+        }
+        else if (student.getHobby() == "sports") {
+            
+        }
+        else if (student.getHobby() == "arts") {
+            
+        }
+        else if (student.getHobby() == "music") {
+            
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+        playList.getEntry(i).getDimension(student.getHobby()).getCategory(1);
+        }
+        
+        
+    } 
 }
