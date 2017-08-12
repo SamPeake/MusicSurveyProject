7181ecc65363721cb6f944990550aa5da5b9804a
Hythams8 <30814005+Hythams8@users.noreply.github.com>
2017-08-11 20:34:37 -0400

    Update GraphSolverTest.java

--- GraphSolverTest.java
+++ GraphSolverTest.java
@@ -3,8 +3,6 @@
  */
 package musicsurvey;
 
-import javax.media.NoDataSourceException;
-
 //import binarySearchTree.ItemNotFoundException;
 import student.TestCase;
 
@@ -14,90 +12,91 @@ import student.TestCase;
  */
 public class GraphSolverTest extends TestCase {
 
-    //fields...............................
+    // fields...............................
     private GraphSolver solver1;
     private GraphSolver solver2;
     private GraphSolver solver3;
     private int pageCounter;
-    
+
     /**
      * this method should get ready all the files
      */
-    public void setUp()
-    {
-        solver1 = new GraphSolver("SongList2016S.csv", "MusicSurveyData2016S.csv");
+    @Override
+    public void setUp() {
+        solver1 = new GraphSolver("SongList2016S.csv",
+                "MusicSurveyData2016S.csv");
         pageCounter = 0;
-        
-    }
-    
-    /**
-     * this method is used to test the makePlayList method 
-     */
-    public void testMakePlayList()
-    {
-       /** 
-        NoDataSourceException thrown = null;
-        try 
-        {
-            solver2.makePlayList();
-        } 
-        catch (NoDataSourceException exception) 
-        {
 
-            thrown = exception;
-        }
-        assertNotNull(thrown);
-        */
     }
-    
-    
+
+    /**
+     * this method is used to test the makePlayList method
+     */
+    public void testMakePlayList() {
+        /**
+         * NoDataSourceException thrown = null; try { solver2.makePlayList(); }
+         * catch (NoDataSourceException exception) { thrown = exception; }
+         * assertNotNull(thrown);
+         */
+        solver1.readSongFile();
+        assertEquals(solver1.getPlayList().size(), 59);
+    }
+
     /**
      * this method is used to test the upDatePlayList
      */
-    public void testUpDatePlayList()
-    {
-        
+    public void testUpDatePlayList() {
+
     }
-    
+
     /**
-     * this method is used to test the sortSong method 
+     * this method is used to test the sortSong method
      */
-    public void testSortSongs()
-    {
-        
+    public void testSortSongs() {
+
+        solver1.readSongFile();
+        solver1.sortSongs("Song");
+        // System.out.println(solver1.getPlayList().removeFirst().toString());
+        String str = "Song Name: All These Things I've Done\n"
+                + "Song Artist: The Killers\n" + "Song Genre: alternative\n"
+                + "Song Year: 2005";
+        assertEquals(str, solver1.getPlayList().removeFirst().toString());
     }
-    
+
     /**
-     * this method is used to test the filpPage method 
+     * this method is used to test the filpPage method
      */
-    public void testFilpPage()
-    {
+    public void testFlipPage() {
         solver1.flipPage(false);
         assertEquals(-1, solver1.getPage());
     }
-    
+
     /**
      * this method is used to test the getPAGE
      */
-    public void testGetPage()
-    {
+    public void testGetPage() {
         solver1.flipPage(true);
         assertEquals(1, solver1.getPage());
     }
-    
+
     /**
      * this method is used to test the getPercenteard
      */
-    public void testgetPercentHeard()
-    {
-        
+    public void testgetPercentHeard() {
+
+        solver1.readSongFile();
+        solver1.getPercentHeard(1, 1, 1);
+        // System.out.println(solver1.getPercentHeard(1, 1, 1));
+        assertEquals(0.0, solver1.getPercentHeard(1, 1, 1), 0.0001);
     }
-    
+
     /**
      * this method is used to test the getPercentLike
      */
-    public void testgetPercentLike()
-    {
-        
+    public void testgetPercentLike() {
+        solver1.readSongFile();
+        solver1.getPercentHeard(1, 1, 1);
+        // System.out.println(solver1.getPercentHeard(1, 1, 1));
+        assertEquals(0.0, solver1.getPercentHeard(1, 1, 1), 0.0001);
     }
 }
